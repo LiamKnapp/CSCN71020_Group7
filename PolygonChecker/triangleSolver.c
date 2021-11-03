a81bdb3253e4563224dacaefae03015be46ac2c0
AndrewStanley6786 <astanley6786@conestogac.on.ca>
2021-11-02 21:42:36 -0400

    Added Function to triangleSolver to calculate the angles of the triangle. Still need to add check for if the triangle is valid.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -4,6 +4,8 @@
 
 #include "triangleSolver.h"
 
+#define RAD_TO_DEG 57.2957795
+
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
@@ -22,4 +24,28 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 
 	return result;
+}
+
+void angleCalculator(int* triangleSides)		// a, b and c used to make the formula easy to read
+{
+	double angleInRad = 0;
+	double angleInDeg = 1;
+	double a = triangleSides[0];
+	double b = triangleSides[1];
+	double c = triangleSides[2];
+
+	angleInRad = acos(((b * b) + (c * c) - (a * a)) / (2 * b * c));	// Law of cosine rearranged to solve for angle A
+	angleInDeg = angleInRad * RAD_TO_DEG;
+
+	printf("%0.3f\n", angleInDeg);
+
+	angleInRad = acos(((a * a) + (c * c) - (b * b)) / (2 * a * c)); // Solve for angle B
+	angleInDeg = angleInRad * RAD_TO_DEG;
+
+	printf("%0.3f\n", angleInDeg);
+
+	angleInRad = acos(((b * b) + (a * a) - (c * c)) / (2 * b * a)); // Solve for angle C
+	angleInDeg = angleInRad * RAD_TO_DEG;
+
+	printf("%0.3f\n", angleInDeg);
 }
