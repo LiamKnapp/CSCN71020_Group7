87c27b59d12cef0f8edba3c229847a8e0aaa1b14
AndrewStanley6786 <astanley6786@conestogac.on.ca>
2021-11-08 13:03:13 -0500

    Trinaglesolver function converted from void to double.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -27,30 +27,41 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	return result;
 }
 
-void angleCalculatorTriangle(int* triangleSides)		// a, b and c used to make the formula easy to read
+//void angleCalculatorTriangle(int* triangleSides)		// a, b and c used to make the variables match the actual math formula
+//{
+//	double angleResult = 0;
+//	double angleInRad  = 0;
+//	double angleAInDeg = 0;
+//	double angleBInDeg = 0;
+//	double angleCInDeg = 0;
+//	double a = triangleSides[0];
+//	double b = triangleSides[1];
+//	double c = triangleSides[2];
+//
+//	angleInRad = acos(((b * b) + (c * c) - (a * a)) / (2 * b * c));	// Law of cosine rearranged to solve for angle A
+//	angleAInDeg = angleInRad * RAD_TO_DEG;
+//
+//	angleInRad = acos(((a * a) + (c * c) - (b * b)) / (2 * a * c)); // Solve for angle B
+//	angleBInDeg = angleInRad * RAD_TO_DEG;
+//
+//	angleInRad = acos(((b * b) + (a * a) - (c * c)) / (2 * b * a)); // Solve for angle C
+//	angleCInDeg = angleInRad * RAD_TO_DEG;
+//
+//	angleResult = round(angleAInDeg + angleBInDeg + angleCInDeg, 0);		// Rounding to make it equal to exactly 180 so it can be checked in the if statement
+//
+//	if (angleResult == VALID_TRIANGLE)	
+//		printf("Angles of this triangle are: %f\n%f\n%f\n", angleAInDeg, angleBInDeg, angleCInDeg);		// Print the angles if they add to 180
+//	else
+//		printf("Angles do not create a triangle\n");		// Print this if the angles do not equal 180
+//}
+
+double angleCalculatorTriangle(int a, int b, int c)
 {
-	double angleResult = 0;
-	double angleInRad  = 0;
-	double angleAInDeg = 0;
-	double angleBInDeg = 0;
-	double angleCInDeg = 0;
-	double a = triangleSides[0];
-	double b = triangleSides[1];
-	double c = triangleSides[2];
+	double angleInRad = 0;
+	double angleInDeg = 0;
+	double value = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
+	angleInRad = acos(value);
+	angleInDeg = angleInRad * RAD_TO_DEG;
 
-	angleInRad = acos(((b * b) + (c * c) - (a * a)) / (2 * b * c));	// Law of cosine rearranged to solve for angle A
-	angleAInDeg = angleInRad * RAD_TO_DEG;
-
-	angleInRad = acos(((a * a) + (c * c) - (b * b)) / (2 * a * c)); // Solve for angle B
-	angleBInDeg = angleInRad * RAD_TO_DEG;
-
-	angleInRad = acos(((b * b) + (a * a) - (c * c)) / (2 * b * a)); // Solve for angle C
-	angleCInDeg = angleInRad * RAD_TO_DEG;
-
-	angleResult = round(angleAInDeg + angleBInDeg + angleCInDeg, 0);		// Rounding to make it equal to exactly 180 so it can be checked in the if statement
-
-	if (angleResult == VALID_TRIANGLE)	
-		printf("Angles of this triangle are: %f\n%f\n%f\n", angleAInDeg, angleBInDeg, angleCInDeg);		// Print the angles if they add to 180
-	else
-		printf("Angles do not create a triangle\n");		// Print this if the angles do not equal 180
+	return angleInDeg;
 }
