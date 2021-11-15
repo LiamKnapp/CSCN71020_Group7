f69968286d36e1ee79b77ad2adfd390201fa1b36
Liam Knapp <Lknapp1110@conestogac.on.ca>
2021-11-15 13:42:10 -0500

    added the test method for test Angle1

--- TextingPolygons/TestingPolygons.cpp
+++ TextingPolygons/TestingPolygons.cpp
@@ -4,7 +4,7 @@
 extern "C" { // call functions from main.c, rectangleSolver.c and triangleSolver.c
 	int* getRectanglePoints(int* rectanglePoints);
 	int* getTriangleSides(int* triangleSides);
-	int angleCalculatorTriangle(int* triangleSides);
+	double angleCalculatorTriangle(int* trianglesides);
 	int checkIfAngle180(double angleA, double angleB, double angleC);
 	char* analyzeTriangle(int side1, int side2, int side3);
 }
@@ -63,7 +63,7 @@ namespace TextingPolygons
 		}
 		TEST_METHOD(TestTriangleAngle180)
 		{
-			// testing to make sure the input is valid and it returns the angle
+			// testing to make sure the input is valid and it returns the sum of the inner angles
 
 			int Result;
 			double angle1 = 60;
@@ -71,6 +71,20 @@ namespace TextingPolygons
 			double angle3 = 60;
 			Result = checkIfAngle180(angle1, angle2, angle3);
 			Assert::AreEqual(TRIINNERANGLE, Result);
+		}
+		TEST_METHOD(TestTriangleAngle1)
+		{
+			// testing to make sure the input is valid and it returns the first angle
+
+			double Result;
+			double angle = 60;
+			int Sides[3];
+			Sides[0] = 5;
+			Sides[1] = 5;
+			Sides[2] = 5;
+
+			Result = angleCalculatorTriangle(Sides);
+			Assert::AreEqual(angle, round(Result));
 		}
 		TEST_METHOD(Test4Points)
 		{
